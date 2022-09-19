8ea3ac65ec7650dfc0149cb506f080d16e1f21e6
R0ota <reham.mohamed.ma@gmailm>
2022-09-20 00:36:39 +0200

    change api key

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const icons = document.querySelectorAll('.entry__icon');
 
 // Base URL and API Key for OpenWeatherMap API
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '&appid=5beb43e13dc3fa8be428191e6e50a00c';
+const apiKey = '&appid=e024755f0cb823ebc12ae3b235e7149e';
 
 //Get the date
 let d = new Date();
